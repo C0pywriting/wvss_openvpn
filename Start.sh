b254a4c3f59ebe5c8b116448d9eb725af237a049
C0pywriting <61686211+C0pywriting@users.noreply.github.com>
2020-03-05 08:04:49 +0100

    Create Start.sh

---
+++ Start.sh
@@ -0,0 +1,8 @@
+wget https://github.com/C0pywriting/wvss_openvpn/archive/master.zip
+apt install unzip
+unzip master.zip
+cd wvss_openvpn-master/
+chmod +x network_client_server.sh
+chmod +x server_vars.sh
+echo "Hier findest du nun die Dateien zum ausführen"
+pwd
